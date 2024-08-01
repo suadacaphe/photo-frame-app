cbd6be3329eb6ebe835b37cee731a394387cb325
suadacaphe <89328335+suadacaphe@users.noreply.github.com>
2024-08-01 10:40:17 +0700

    Update server.cjs

--- my-backend/server.cjs
+++ my-backend/server.cjs
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:8080", // Replace with your Vue app's origin
+    origin: "https://suadacaphe.github.io/photo-frame-app/", // Replace with your Vue app's origin
     credentials: true,
   })
 );
